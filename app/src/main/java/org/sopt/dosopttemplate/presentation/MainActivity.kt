836466aa4fe022056312d879ad7c77e5898cbcf9
Jokwanhee <jonotch1@naver.com>
2023-10-10 15:03:12 +0900

    [mod] resultActivity name and add extensions func

--- app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/MainActivity.kt
@@ -7,14 +7,11 @@ import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.base.BaseActivity
 import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 import org.sopt.dosopttemplate.model.User
+import org.sopt.dosopttemplate.util.getParcelableData
 
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun initView() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            intent.getParcelableExtra("user", User::class.java)
-        } else {
-            intent.getParcelableExtra("user")
-        }?.let {
+        intent.getParcelableData("user", User::class.java)?.let {
             binding.tvProfileNickname.text = it.nickname
             binding.tvIdContent.text = it.id
             binding.tvMbtiContent.text = it.mbti.toString()
--- app/src/main/java/org/sopt/dosopttemplate/util/Extensions.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/Extensions.kt
@@ -1,8 +1,14 @@
 package org.sopt.dosopttemplate.util
 
+import android.content.Intent
+import android.os.Build
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showShortSnackBar(msg: String) {
     Snackbar.make(this, msg, Snackbar.LENGTH_SHORT).show()
 }
+
+fun <T> Intent.getParcelableData(name: String, className: Class<T>): T? =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) this.getParcelableExtra(name, className)
+    else this.getParcelableExtra(name)
