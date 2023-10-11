57db92ca8eaf8f53796ab2f5d0c46e86a7e3a590
Jokwanhee <jonotch1@naver.com>
2023-10-11 16:56:01 +0900

    week1 advanced assignment

--- app/src/main/java/org/sopt/dosopttemplate/util/Extensions.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/Extensions.kt
@@ -1,8 +1,11 @@
 package org.sopt.dosopttemplate.util
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showShortSnackBar(msg: String) {
@@ -12,3 +15,12 @@ fun View.showShortSnackBar(msg: String) {
 fun <T> Intent.getParcelableData(name: String, className: Class<T>): T? =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) this.getParcelableExtra(name, className)
     else this.getParcelableExtra(name)
+
+
+fun Activity.hideKeyboard(view: View) {
+    (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).let { imm ->
+        if (imm.isAcceptingText) {
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
