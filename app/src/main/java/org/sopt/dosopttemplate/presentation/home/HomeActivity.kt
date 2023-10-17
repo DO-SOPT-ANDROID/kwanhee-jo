a29fceb8bec65738861207239ca18dc9cac43932
Jokwanhee <jonotch1@naver.com>
2023-10-18 01:07:33 +0900

    [add] fragment and bottomnavigation setting
    프래그먼트 및 바텀내비게이션 셋팅

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeActivity.kt
@@ -1,10 +1,9 @@
 package org.sopt.dosopttemplate.presentation
 
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.base.BaseActivity
-import org.sopt.dosopttemplate.databinding.ActivityMainBinding
+import org.sopt.dosopttemplate.databinding.ActivityHomeBinding
 import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.AUTO_LOGIN
 import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.ID
 import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.MBTI
@@ -12,7 +11,7 @@ import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.NICKNAME
 import org.sopt.dosopttemplate.presentation.LoginActivity.Companion.getSharedPreferenceUser
 import org.sopt.dosopttemplate.util.showShortToastMessage
 
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate) {
     private var backPressedTime = 0L
 
     override fun initView() {
@@ -38,7 +37,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
             override fun handleOnBackPressed() {
                 if (System.currentTimeMillis() - backPressedTime >= 2000L) {
                     backPressedTime = System.currentTimeMillis()
-                    this@MainActivity.showShortToastMessage(getString(R.string.finish_dialog))
+                    this@HomeActivity.showShortToastMessage(getString(R.string.finish_dialog))
                 } else {
                     finish()
                 }
