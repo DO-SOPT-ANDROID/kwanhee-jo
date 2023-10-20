daef0fe350fa326460d21c6628e2427133dee64c
Jokwanhee <jonotch1@naver.com>
2023-10-20 10:25:02 +0900

    [add] 상호님 임시 리스트 파일 피드백
    테스트 더미 리스트 데이터를 따로 파일로 빼서 사용하는것을 말씀해주셔서, 피드백 수용!

--- app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/home/HomeFragment.kt
@@ -12,12 +12,12 @@ import org.sopt.dosopttemplate.model.Music
 import org.sopt.dosopttemplate.model.Profile
 import org.sopt.dosopttemplate.model.ProfileBirthday
 import org.sopt.dosopttemplate.model.ProfileHeader
+import org.sopt.dosopttemplate.util.sampleDeque
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val layoutId: Int
         get() = R.layout.fragment_home
     private lateinit var homeAdapter: HomeAdapter
-    private var testDeque = ArrayDeque<HomeProfileModel>()
     private var birthdayDeque = ArrayDeque<ProfileBirthday>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,112 +25,17 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         initHomeAdapter()
         initList()
         setBirthdayContent()
-        homeAdapter.setProfileList(testDeque.toList())
+        homeAdapter.setProfileList(sampleDeque.toList())
     }
 
     private fun setBirthdayContent() {
         birthdayDeque.forEach {
-            testDeque.add(1, it)
+            sampleDeque.add(1, it)
         }
     }
 
     private fun initList() {
-        testDeque.apply {
-            add(ProfileHeader("조관희", "졸리다", R.drawable.jokwanhee))
-            add(
-                Profile(
-                    "SOPT YB 이상해씨",
-                    "이상해씨!!!",
-                    R.drawable.leaf1,
-                    1697565421000,
-                    1697565421000,
-                    Music("야생화", "박효신")
-                )
-            )
-            add(Profile("SOPT YB 이상해풀", "이상해풀!!!", R.drawable.leaf2, 0, 1696565421000, null))
-            add(
-                Profile(
-                    "SOPT YB 이상해꽃 이름이 길어진다 길어진다 길어진다",
-                    "이상해꽃 길어진다 길어진다 길어진다 길어진다 길어진다 길어진다",
-                    R.drawable.leaf3,
-                    0,
-                    1697565421000,
-                    Music("대장", "박효신")
-                )
-            )
-            add(Profile("SOPT YB 파이리", "파이리", R.drawable.re1, 1697610124000, 1697565421000, null))
-            add(Profile("SOPT YB 리자드", "리자드", R.drawable.re2, 0, 1697565421000, null))
-            add(
-                Profile(
-                    "SOPT YB 리자몽",
-                    "리자몽",
-                    R.drawable.re3,
-                    1697695757000,
-                    1697695757000,
-                    Music("나도", "박효신")
-                )
-            )
-            add(
-                Profile(
-                    "SOPT YB 꼬부기",
-                    "꼬부기",
-                    R.drawable.turtle,
-                    1697695757000,
-                    1697695757000,
-                    Music("몰라", "박효신")
-                )
-            )
-            add(
-                Profile(
-                    "SOPT YB 어니부기",
-                    "어니부기",
-                    R.drawable.turtlemiddle,
-                    1697565421000,
-                    1697565421000,
-                    null
-                )
-            )
-            add(
-                Profile(
-                    "SOPT YB 거북왕",
-                    "거북왕",
-                    R.drawable.turtleking,
-                    1697695757000,
-                    1697695757000,
-                    Music("노래", "박효신")
-                )
-            )
-            add(
-                Profile(
-                    "SOPT YB 피카츄",
-                    "피카츄",
-                    R.drawable.pickchu,
-                    1697565421000,
-                    1697565421000,
-                    null
-                )
-            )
-            add(
-                Profile(
-                    "SOPT YB 라이츄",
-                    "라이츄",
-                    R.drawable.raichu,
-                    1697565421000,
-                    1697565421000,
-                    Music("노래 제목이 길게하면 어떨까?", "박효신")
-                )
-            )
-            add(
-                Profile(
-                    "이름이 길면 어떨까???? 어떨까???? 어떨까????",
-                    "글씨가 길면 어떨까? 어떨까? 어떨까? 어떨까? 어떨까? 어떨까? 어떨까?",
-                    null,
-                    1697565421000,
-                    1697565421000,
-                    null
-                )
-            )
-        }.forEach {
+        sampleDeque.forEach {
             if (it is Profile && it.checkBirthDay()) {
                 birthdayDeque.apply {
                     add(ProfileBirthday(it.name, it.description, it.profileImage))
---
+++ app/src/main/java/org/sopt/dosopttemplate/util/SampleList.kt
@@ -0,0 +1,104 @@
+package org.sopt.dosopttemplate.util
+
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.model.HomeProfileModel
+import org.sopt.dosopttemplate.model.Music
+import org.sopt.dosopttemplate.model.Profile
+import org.sopt.dosopttemplate.model.ProfileHeader
+
+val sampleDeque = ArrayDeque<HomeProfileModel>().apply {
+    add(ProfileHeader("조관희", "졸리다", R.drawable.jokwanhee))
+    add(
+        Profile(
+            "SOPT YB 이상해씨",
+            "이상해씨!!!",
+            R.drawable.leaf1,
+            1697565421000,
+            1697565421000,
+            Music("야생화", "박효신")
+        )
+    )
+    add(Profile("SOPT YB 이상해풀", "이상해풀!!!", R.drawable.leaf2, 0, 1696565421000, null))
+    add(
+        Profile(
+            "SOPT YB 이상해꽃 이름이 길어진다 길어진다 길어진다",
+            "이상해꽃 길어진다 길어진다 길어진다 길어진다 길어진다 길어진다",
+            R.drawable.leaf3,
+            0,
+            1697565421000,
+            Music("대장", "박효신")
+        )
+    )
+    add(Profile("SOPT YB 파이리", "파이리", R.drawable.re1, 1697610124000, 1697565421000, null))
+    add(Profile("SOPT YB 리자드", "리자드", R.drawable.re2, 0, 1697565421000, null))
+    add(
+        Profile(
+            "SOPT YB 리자몽",
+            "리자몽",
+            R.drawable.re3,
+            1697695757000,
+            1697695757000,
+            Music("나도", "박효신")
+        )
+    )
+    add(
+        Profile(
+            "SOPT YB 꼬부기",
+            "꼬부기",
+            R.drawable.turtle,
+            1697695757000,
+            1697695757000,
+            Music("몰라", "박효신")
+        )
+    )
+    add(
+        Profile(
+            "SOPT YB 어니부기",
+            "어니부기",
+            R.drawable.turtlemiddle,
+            1697565421000,
+            1697565421000,
+            null
+        )
+    )
+    add(
+        Profile(
+            "SOPT YB 거북왕",
+            "거북왕",
+            R.drawable.turtleking,
+            1697695757000,
+            1697695757000,
+            Music("노래", "박효신")
+        )
+    )
+    add(
+        Profile(
+            "SOPT YB 피카츄",
+            "피카츄",
+            R.drawable.pickchu,
+            1697565421000,
+            1697565421000,
+            null
+        )
+    )
+    add(
+        Profile(
+            "SOPT YB 라이츄",
+            "라이츄",
+            R.drawable.raichu,
+            1697565421000,
+            1697565421000,
+            Music("노래 제목이 길게하면 어떨까?", "박효신")
+        )
+    )
+    add(
+        Profile(
+            "이름이 길면 어떨까???? 어떨까???? 어떨까????",
+            "글씨가 길면 어떨까? 어떨까? 어떨까? 어떨까? 어떨까? 어떨까? 어떨까?",
+            null,
+            1697565421000,
+            1697565421000,
+            null
+        )
+    )
+}
